583a4125da4945155a5710e7a5c6fbe9267cfd8d
shojol021 <shojol021@gmail.com>
2023-04-24 19:01:24 +0600

    updated

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import logo from '../../assets/logo.png'
+import moment from 'moment/moment';
 
 const Header = () => {
     return (
         <div className='text-center'>
             <img src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
-            <p>date</p>
+            <p>{moment().format("ddd, MMMM DD, YYYY")}</p>
         </div>
     );
 };
