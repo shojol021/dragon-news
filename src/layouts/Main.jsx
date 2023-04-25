10c5ab15e9ff34e43dc224d01de6b0b981fe224d
shojol021 <shojol021@gmail.com>
2023-04-25 15:54:35 +0600

    right nav updated

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import Header from '../pages/shared/Header';
 import Footer from '../pages/shared/Footer';
-import { Outlet } from 'react-router-dom';
 import { Col, Container, Row } from 'react-bootstrap';
 import RightNav from '../pages/shared/RightNav';
 import LeftNave from '../pages/shared/LeftNave';
@@ -12,9 +10,9 @@ const Main = () => {
             <Header></Header>
             <Container>
                 <Row>
-                    <Col><RightNav></RightNav></Col>
-                    <Col lg={6}>Page Content</Col>
                     <Col><LeftNave></LeftNave></Col>
+                    <Col lg={6}>Page Content</Col>
+                    <Col><RightNav></RightNav></Col>
                 </Row>
             </Container>
             <Footer></Footer>
--- src/pages/shared/RightNav.jsx
+++ src/pages/shared/RightNav.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
-            Right Nav
+            <div>
+                <h4 className='fw-bolder'>Login With</h4>
+                <Button className='my-2 w-100' variant="outline-success"><FaGoogle></FaGoogle> Login with Google</Button>{' '}
+                <Button className='my-2 w-100' variant="outline-dark"><FaGithub></FaGithub> Login with Github</Button>
+            </div>
+            <div>
+                <div>
+                    <h4 className='fw-bolder mt-4'>Find Us On</h4>
+                    <ButtonGroup vertical className='w-100'>
+                        <Button className='bg-white text-dark border border-secondary'><FaFacebook className='text-primary'></FaFacebook> Facebook</Button>
+                        <Button className='bg-white text-dark border border-secondary'><FaTwitter className='text-info'></FaTwitter> Twitter</Button>
+                        <Button className='bg-white text-dark border border-secondary'><FaInstagram className='text-danger'></FaInstagram> Instagram</Button>
+                    </ButtonGroup>
+                </div>
+            </div>
         </div>
     );
 };
