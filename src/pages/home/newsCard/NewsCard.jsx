bcc8ed9c873a6f063ba777f10c6232c95e18b37b
shojol021 <shojol021@gmail.com>
2023-04-27 01:26:50 +0600

    updated single news and header

--- src/pages/home/newsCard/NewsCard.jsx
+++ src/pages/home/newsCard/NewsCard.jsx
@@ -1,7 +1,7 @@
 import { Button, Card, Image } from "react-bootstrap";
 import Rating from "react-rating";
 import { Link } from "react-router-dom";
-import { FaRegEye } from "react-icons/fa";
+import { FaRegBookmark, FaRegEye, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
 import moment from "moment/moment";
 
 const NewsCard = ({ news }) => {
@@ -10,30 +10,34 @@ const NewsCard = ({ news }) => {
     return (
 
         <Card className="mb-4">
-            <Card.Header className="d-flex">
-                <Image roundedCircle src={author.img} style={{ height: '40px' }}></Image>
-                <div className="ms-2">
-                    <p className="m-0">{author.name}</p>
-                    <p className="m-0">{moment(author.published_date).format('YYYY-MM-DD')}</p>
+            <Card.Header className="d-flex align-items-center">
+                <Image roundedCircle src={author?.img} style={{ height: '40px' }}></Image>
+                <div className="ms-2 flex-grow-1">
+                    <p className="m-0">{author?.name}</p>
+                    <p className="m-0">{moment(author?.published_date).format('YYYY-MM-DD')}</p>
+                </div>
+                <div>
+                    <FaRegBookmark></FaRegBookmark>
+                    <FaShareAlt></FaShareAlt>
                 </div>
             </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Img variant="top" src={image_url} />
                 <Card.Text>
-                    {details.length < 250 ? details :
-                        `${details.slice(0, 250)}...`}
-
+                    {details.length <= 250 ?
+                        details : details.slice(0, details.lastIndexOf(' ', 250))}...
                     <Link to={`/news/${_id}`}><Button variant="link">Read more</Button></Link>
                 </Card.Text>
 
             </Card.Body>
             <Card.Footer className="text-muted d-flex">
                 <Rating className="flex-grow-1"
-                    placeholderRating={3.5}
-                    emptySymbol={<img src="assets/images/star-grey.png" className="icon" />}
-                    placeholderSymbol={<img src="assets/images/star-red.png" className="icon" />}
-                    fullSymbol={<img src="assets/images/star-yellow.png" className="icon" />}
+                    readonly
+                    placeholderRating={rating}
+                    emptySymbol={<FaRegStar></FaRegStar>}
+                    placeholderSymbol={<FaStar></FaStar>}
+                    fullSymbol={<FaStar></FaStar>}
                 />
 
                 <div className="d-flex">
